6218ae584e27e40ea277eb42408ee83b1d79b8d2
cafekonnect <cafekonnect@gmail.com>
2019-06-28 19:45:05 +0300

    first commit

---
+++ interview/app.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const path = require('path');
+const Joi = require('joi');
+
+const db = require("./db");
+const collection = "item";
+const app = express();
+
+// schema used for data validation for our item document
+const schema = Joi.object().keys({
+    item : Joi.string().required()
+});
+
+// parses json data sent to us by the user 
+app.use(bodyParser.json());
+
+// serve static html file to user
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+
+// read
+app.get('/getRestaurants',(req,res)=>{
+    // get all  documents within our item collection
+    // send back to user as json
+    db.getDB().collection(collection).find({}).toArray((err,documents)=>{
+        if(err)
+            console.log(err);
+        else{
+            res.json(documents);
+        }
+    });
+});
+
+
+
+
+//create
+app.post('/',(req,res,next)=>{
+    // Document to be inserted
+    const userInput = req.body;
+
+    // Validate document
+    // If document is invalid pass to error middleware
+    // else insert document within item collection
+    Joi.validate(userInput,schema,(err,result)=>{
+        if(err){
+            const error = new Error("Invalid Input");
+            error.status = 400;
+            next(error);
+        }
+        else{
+            db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+                if(err){
+                    const error = new Error("Failed to insert Document");
+                    error.status = 400;
+                    next(error);
+                }
+                else
+                    res.json({result : result, document : result.ops[0],msg : "Successfully inserted !!!",error : null});
+            });
+        }
+    })    
+});
+
+
+
+
+// Middleware for handling Error
+// Sends Error Response Back to User
+app.use((err,req,res,next)=>{
+    res.status(err.status).json({
+        error : {
+            message : err.message
+        }
+    });
+})
+
+
+db.connect((err)=>{
+    // If err unable to connect to database
+    // End application
+    if(err){
+        console.log('unable to connect to database');
+        process.exit(1);
+    }
+    // Successfully connected to database
+    // Start up our Express Application
+    // And listen for Request
+    else{
+        app.listen(3000,()=>{
+            console.log('connected to database, app listening on port 3000');
+        });
+    }
+});
+
